a9853a5e00c8d8d61c6413e5fff92a156ca88f7b
Becki Srofe <blf_23@yahoo.com>
2017-08-06 15:17:00 -0600

    Start I3

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,5 +4,14 @@ require 'sinatra/reloader'
 number = rand(101)
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess/"].to_i
+  message = check_nums(guess, number)
+  erb :index, :locals => {:number => number, :message => message}
+end
+
+def check_nums(guess, number)
+  if guess > number
+    message = "Too high!"
+  end
+  message
 end
